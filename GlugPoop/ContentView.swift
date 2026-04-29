ce6a17f0a333738c363980486bf2cae80b1a6c1b
Fitchchiu <fitchchiu@gmail.com>
2026-04-29 16:43:08 +0800

    Initial Commit

---
+++ GlugPoop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GlugPoop
+//
+//  Created by feng on 2026/4/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GlugPoop/GlugPoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlugPoopApp.swift
+//  GlugPoop
+//
+//  Created by feng on 2026/4/29.
+//
+
+import SwiftUI
+
+@main
+struct GlugPoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
